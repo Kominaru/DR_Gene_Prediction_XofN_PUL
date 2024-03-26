Fix leakage bug and add neptune logging of best pu learning hyperparams

Code/model_training.py
@@ -68,7 +68,12 @@ def cv_train_with_params(
             neptune_run["metrics/fs_time_overhead"].append(time.time() - t1)
             
             x_learn, y_learn = pu_learning.select_reliable_negatives(
-                x_train, y_train, params["pu_learning"] ,params["pu_k"], params["pu_t"]
+                x_learn, y_learn, params["pu_learning"] ,params["pu_k"], params["pu_t"]
+            )
+
+            # Log the number of reliable negatives
+            neptune_run["metrics/innercv_num_reliable_negatives"].append(
+                np.sum(y_learn == 0)/len(y_learn)
             )
 
         x_learn_feat, x_val_feat = generate_features(

Code/pu_learning.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import sklearn
 from code.data_processing import get_data_features
@@ -133,6 +134,8 @@ def select_reliable_negatives(
         p = x[y == 1]
         u = x[y == 0]
 
+        u = u[np.random.permutation(len(u))]
+
         # Split u into k subsets
         u_split = np.array_split(u, k)
 
@@ -146,17 +149,13 @@ def select_reliable_negatives(
 
             x_i_feat = get_data_features(x_i)
 
-            model = RandomForestClassifier(random_state=random_state)
+            model = RandomForestClassifier(random_state=random_state, min_samples_leaf=5)
             model.fit(x_i_feat, y_i)
 
             u_i_feat = get_data_features(u_split[i])
+            p_feat = get_data_features(p)
 
             model_predictions = model.predict_proba(u_i_feat)[:, 1]
-
-            # Print the average probability of the positive class
-            # print(np.mean(model.predict_proba(x_i_feat)[np.where(y_i == 1), 1]))
-            # # Print the average probability of the negative class
-            # print(np.mean(model.predict_proba(x_i_feat)[np.where(y_i == 0), 1]))
 
             rn.append(u_split[i][model_predictions <= t])
 